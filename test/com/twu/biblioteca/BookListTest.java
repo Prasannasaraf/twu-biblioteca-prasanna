d6e1a68e567bd95a9ed6f4c5e6b9a9a8fd7f9a3b
Prasannasaraf <prasannasaraf180@gmail.com>
2015-07-28 12:42:27 +0530

    Book gives book name

---
+++ test/com/twu/biblioteca/BookListTest.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.BookList;
+import org.junit.Test;
+
+public class BookListTest {
+    @Test
+    public void shouldGiveListOfBooks() {
+        BookList bookList = new BookList();
+    }
+}

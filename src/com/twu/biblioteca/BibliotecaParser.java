1d7f072c6b09ba2d6de1c24f770f043baee5dda3
Prasannasaraf <prasannasaraf180@gmail.com>
2015-07-30 15:03:01 +0530

    |#020| Added Quit and Invalid option and checkout in parser

--- src/com/twu/biblioteca/BibliotecaParser.java
+++ src/com/twu/biblioteca/BibliotecaParser.java
@@ -3,9 +3,13 @@ package com.twu.biblioteca;
 public class BibliotecaParser {
     public Operations parse(String userInput, View view, Library library) {
         switch (userInput) {
+            case "0":
+                return new Quit();
             case "1":
                 return new DisplayBooks(view,library);
+            default:
+                return new InvalidOption(view);
         }
-        return null;
+
     }
 }

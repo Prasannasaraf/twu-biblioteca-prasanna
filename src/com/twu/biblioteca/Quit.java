4e0c2c380130a1d692dabfb4a89e03a885f13812
Prasannasaraf <prasannasaraf180@gmail.com>
2015-07-30 15:05:58 +0530

    |#021| Added Quit to Parser

---
+++ src/com/twu/biblioteca/Quit.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+public class Quit implements Operations {
+    public Quit() {
+    }
+
+    @Override
+    public void execute() {
+        System.exit(0);
+    }
+}

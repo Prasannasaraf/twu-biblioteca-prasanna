897c9f0e011bfc3e43f030aa1cc78146ad66258f
Prasannasaraf <prasannasaraf180@gmail.com>
2015-08-06 02:52:03 +0530

    |#072| Flow from Login to UserController

--- src/com/twu/biblioteca/model/Login.java
+++ src/com/twu/biblioteca/model/Login.java
@@ -1,25 +1,47 @@
 package com.twu.biblioteca.model;
 
+import com.twu.biblioteca.controller.BibliotecaParser;
+import com.twu.biblioteca.operations.Operations;
+import com.twu.biblioteca.presentation.Messages;
 import com.twu.biblioteca.presentation.View;
 
 import java.util.ArrayList;
 
-public class Login {
+public class Login implements Operations {
     private final ArrayList<User> users;
     private final View view;
+    private final BibliotecaParser parser;
+    private Library bookLibrary;
+    private Library movieLibrary;
 
-    public Login(ArrayList<User> users, View view) {
+    public Login(ArrayList<User> users, View view, BibliotecaParser parser, Library bookLibrary, Library movieLibrary) {
         this.users = users;
         this.view = view;
+        this.parser = parser;
+        this.bookLibrary = bookLibrary;
+        this.movieLibrary = movieLibrary;
     }
 
     public User authenticate() {
+        view.show(Messages.enterUserName);
         String libraryNo = view.getInput();
+        view.show(Messages.enterPassword);
         String password = view.getInput();
         for (User user : users) {
             if (user.match(libraryNo, password))
                 return user;
         }
         return null;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public void execute() {
+        User user = authenticate();
+        Operations operations = parser.parse(view, bookLibrary, movieLibrary, user, this);
+        operations.execute();
     }
 }
--- test/com/twu/biblioteca/model/LoginTest.java
+++ test/com/twu/biblioteca/model/LoginTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca.model;
 
+import com.twu.biblioteca.controller.BibliotecaParser;
 import com.twu.biblioteca.presentation.View;
 import org.junit.Test;
 
@@ -15,12 +16,15 @@ public class LoginTest {
     @Test
     public void shouldGiveTheUserObject() {
         View view = mock(View.class);
+        BibliotecaParser parser = mock(BibliotecaParser.class);
+        Library bookLibrary = mock(Library.class);
+        Library movieLibrary = mock(Library.class);
         ArrayList<User> users = new ArrayList<>();
         User user1 = new User("Ram", "ramprasad@twu.com", "619", "123-4567", "ramRam", true);
         User user2 = new User("Laxman", "laxmanrasad@twu.com", "916", "765-4321", "laxMan", true);
         users.add(user1);
         users.add(user2);
-        Login login = new Login(users, view);
+        Login login = new Login(users, view, parser, bookLibrary, movieLibrary);
 
         when(view.getInput()).thenReturn("123-4567").thenReturn("ramRam");
         assertEquals(user1, login.authenticate());

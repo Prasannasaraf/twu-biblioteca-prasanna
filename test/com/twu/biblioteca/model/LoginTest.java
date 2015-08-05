0f95e7c1ed4b60904a8c10075eb2a9ee9b0cc498
Prasannasaraf <prasannasaraf180@gmail.com>
2015-08-05 19:06:55 +0530

    |#056| Login will authenticate the user

---
+++ test/com/twu/biblioteca/model/LoginTest.java
@@ -0,0 +1,28 @@
+package com.twu.biblioteca.model;
+
+import com.twu.biblioteca.presentation.View;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class LoginTest {
+
+    @Test
+    public void shouldGiveTheUserObject() {
+        View view = mock(View.class);
+        ArrayList<User> users = new ArrayList<>();
+        User user1 = new User("Ram", "ramprasad@twu.com", "619", "123-4567", "ramRam", true);
+        User user2 = new User("Laxman", "laxmanrasad@twu.com", "916", "765-4321", "laxMan", true);
+        users.add(user1);
+        users.add(user2);
+        Login login = new Login(users, view);
+
+        when(view.getInput()).thenReturn("123-4567").thenReturn("ramRam");
+        assertEquals(user1, login.authenticate());
+    }
+}

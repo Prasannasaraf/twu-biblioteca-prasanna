64de935944ba0bed1c67d4ceeb33944275e9b808
Prasannasaraf <prasannasaraf180@gmail.com>
2015-08-05 19:08:53 +0530

    |#057| Created Null object which does nothing

---
+++ src/com/twu/biblioteca/controller/NullObject.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca.controller;
+
+import com.twu.biblioteca.operations.Operations;
+
+public class NullObject implements Operations {
+
+    @Override
+    public void execute() {
+    }
+}

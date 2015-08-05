7cf4575fc4b2a4936013a9fffd4a3ba6072270eb
Prasannasaraf <prasannasaraf180@gmail.com>
2015-08-06 02:12:55 +0530

    |#066| Display User Information

--- src/com/twu/biblioteca/controller/BibliotecaParser.java
+++ src/com/twu/biblioteca/controller/BibliotecaParser.java
@@ -2,32 +2,34 @@ package com.twu.biblioteca.controller;
 
 
 import com.twu.biblioteca.model.Library;
+import com.twu.biblioteca.model.Login;
+import com.twu.biblioteca.model.User;
 import com.twu.biblioteca.operations.*;
 import com.twu.biblioteca.presentation.Messages;
 import com.twu.biblioteca.presentation.View;
 
 //Returns Domain Objects
 public class BibliotecaParser {
-    public Operations parse(String userInput, View view, Library bookLibrary, Library moviesLibrary) {
+    public Operations parse(String userInput, View view, Library bookLibrary, Library moviesLibrary, User user, Login login) {
         switch (userInput) {
             case "0":
                 return new Quit();
             case "1":
                 return new Display(view, bookLibrary.getItems(), Messages.listOfBooks, Messages.booksHeader);
             case "2":
-                return new Display(view, bookLibrary.getCheckedOutItems(), Messages.listOfCheckedOutBooks, Messages.booksHeader);
+                return new CheckOut(view, bookLibrary, Messages.enterBookName, Messages.successfulBookCheckout, Messages.unsuccessfulBookCheckOut, user);
             case "3":
-                return new CheckOut(view, bookLibrary, Messages.enterBookName, Messages.successfulBookCheckout, Messages.unsuccessfulBookCheckOut);
+                return new CheckIn(view, bookLibrary, Messages.enterBookName, Messages.successfulBookCheckIn, Messages.unsuccessfulBookCheckIn, user);
             case "4":
-                return new CheckIn(view, bookLibrary, Messages.enterBookName, Messages.successfulBookCheckIn, Messages.unsuccessfulBookCheckIn);
-            case "5":
                 return new Display(view, moviesLibrary.getItems(), Messages.listOfMovies, Messages.moviesHeader);
+            case "5":
+                return new CheckOut(view, moviesLibrary, Messages.enterMovieName, Messages.successfulMovieCheckout, Messages.unsuccessfulMovieCheckOut, user);
             case "6":
-                return new Display(view, moviesLibrary.getCheckedOutItems(), Messages.listOfCheckedOutMovies, Messages.moviesHeader);
+                return new CheckIn(view, moviesLibrary, Messages.enterMovieName, Messages.successfulMovieCheckIn, Messages.unsuccessfulMovieCheckIn, user);
             case "7":
-                return new CheckOut(view, moviesLibrary, Messages.enterMovieName, Messages.successfulMovieCheckout, Messages.unsuccessfulMovieCheckOut);
+                return new DisplayUserInformation(view, user);
             case "8":
-                return new CheckIn(view, moviesLibrary, Messages.enterMovieName, Messages.successfulMovieCheckIn, Messages.unsuccessfulMovieCheckIn);
+                return new DisplayCheckedOutList(view, bookLibrary.getCheckedOutItems(), Messages.listOfCheckedOutBooks, Messages.checkedOutBooksHeader);
             default:
                 return new InvalidOption(view);
         }
--- test/com/twu/biblioteca/controller/BibliotecaParserTest.java
+++ test/com/twu/biblioteca/controller/BibliotecaParserTest.java
@@ -1,13 +1,16 @@
 package com.twu.biblioteca.controller;
 
 import com.twu.biblioteca.model.Library;
+import com.twu.biblioteca.model.Login;
+import com.twu.biblioteca.model.User;
 import com.twu.biblioteca.operations.*;
 import com.twu.biblioteca.presentation.View;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BibliotecaParserTest {
 
@@ -15,51 +18,68 @@ public class BibliotecaParserTest {
     BibliotecaParser bibliotecaParser;
     View view;
     Library library;
+    User user;
+    Login login;
 
     @Before
     public void setUp() {
         bibliotecaParser = new BibliotecaParser();
-        view = Mockito.mock(View.class);
-        library = Mockito.mock(Library.class);
-        moviesLibrary = Mockito.mock(Library.class);
-    }
-
-    @Test
-    public void shouldReturnDisplayBooksObject() {
-        assertEquals(Display.class, bibliotecaParser.parse("1", view, library, moviesLibrary).getClass());
-    }
-    @Test
-    public void shouldReturnDisplayMoviesObject() {
-        assertEquals(Display.class, bibliotecaParser.parse("5", view, library, moviesLibrary).getClass());
-    }
-
-    @Test
-    public void shouldGiveInvalidOptionObject() {
-        assertEquals(InvalidOption.class, bibliotecaParser.parse("90", view, library, moviesLibrary).getClass());
+        view = mock(View.class);
+        library = mock(Library.class);
+        moviesLibrary = mock(Library.class);
+        user = mock(User.class);
+        login = mock(Login.class);
     }
 
     @Test
     public void shouldGiveQuitObject() {
-        assertEquals(Quit.class, bibliotecaParser.parse("0", view, library, moviesLibrary).getClass());
+        assertEquals(Quit.class, bibliotecaParser.parse("0", view, library, moviesLibrary, user, login).getClass());
+    }
+
+    @Test
+    public void shouldReturnDisplayBooksObject() {
+        assertEquals(Display.class, bibliotecaParser.parse("1", view, library, moviesLibrary, user, login).getClass());
     }
 
     @Test
     public void shouldGiveCheckoutObject() {
-        assertEquals(CheckOut.class, bibliotecaParser.parse("3", view, library, moviesLibrary).getClass());
-    }
-
-    @Test
-    public void shouldGiveCheckoutObjectOn7AsInput() {
-        assertEquals(CheckOut.class, bibliotecaParser.parse("7", view, library, moviesLibrary).getClass());
+        assertEquals(CheckOut.class, bibliotecaParser.parse("2", view, library, moviesLibrary, user, login).getClass());
     }
 
     @Test
     public void shouldGiveCheckInObject() {
-        assertEquals(CheckIn.class, bibliotecaParser.parse("4", view, library, moviesLibrary).getClass());
+        assertEquals(CheckIn.class, bibliotecaParser.parse("3", view, library, moviesLibrary, user, login).getClass());
     }
 
     @Test
-    public void shouldGiveCheckInObjectOn8AsInput() {
-        assertEquals(CheckIn.class, bibliotecaParser.parse("8", view, library, moviesLibrary).getClass());
+    public void shouldReturnDisplayMoviesObject() {
+        assertEquals(Display.class, bibliotecaParser.parse("4", view, library, moviesLibrary, user, login).getClass());
+    }
+
+    @Test
+    public void shouldGiveCheckoutObjectOn5AsInput() {
+        assertEquals(CheckOut.class, bibliotecaParser.parse("5", view, library, moviesLibrary, user, login).getClass());
+    }
+
+    @Test
+    public void shouldGiveCheckInObjectOn6AsInput() {
+        assertEquals(CheckIn.class, bibliotecaParser.parse("6", view, library, moviesLibrary, user, login).getClass());
+    }
+
+    @Test
+    public void shouldGiveDisplayUserInformationObjectOn7AsInput() {
+        assertEquals(DisplayUserInformation.class, bibliotecaParser.parse("7", view, library, moviesLibrary, user, login).getClass());
+    }
+
+    @Test
+    public void shouldGiveDisplayCheckOutListObjectOn8AsInput() {
+        when(user.isLibrarian()).thenReturn(true);
+
+        assertEquals(DisplayCheckedOutList.class, bibliotecaParser.parse("8", view, library, moviesLibrary, user, login).getClass());
+    }
+
+    @Test
+    public void shouldGiveInvalidOptionObject() {
+        assertEquals(InvalidOption.class, bibliotecaParser.parse("90", view, library, moviesLibrary, user, login).getClass());
     }
 }

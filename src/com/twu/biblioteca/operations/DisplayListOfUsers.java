31d2df2ab1a1df159c12bb4251ca4462da3da7e1
Prasannasaraf <prasannasaraf180@gmail.com>
2015-08-05 23:41:35 +0530

    |#065| DisplayListOfUsers command Has been Implemented

---
+++ src/com/twu/biblioteca/operations/DisplayListOfUsers.java
@@ -0,0 +1,26 @@
+package com.twu.biblioteca.operations;
+
+import com.twu.biblioteca.model.User;
+import com.twu.biblioteca.presentation.Messages;
+import com.twu.biblioteca.presentation.View;
+
+import java.util.ArrayList;
+
+public class DisplayListOfUsers implements Operations {
+    private final View view;
+    private final ArrayList<User> users;
+    private final Messages listOfUsers;
+    private final Messages userHeader;
+
+    public DisplayListOfUsers(View view, ArrayList<User> users, Messages listOfUsers, Messages userHeader) {
+        this.view = view;
+        this.users = users;
+        this.listOfUsers = listOfUsers;
+        this.userHeader = userHeader;
+    }
+
+    @Override
+    public void execute() {
+        view.displayListOfUsers(users,listOfUsers,userHeader);
+    }
+}
---
+++ test/com/twu/biblioteca/operations/DisplayListOfUsersTest.java
@@ -0,0 +1,25 @@
+package com.twu.biblioteca.operations;
+
+import com.twu.biblioteca.model.User;
+import com.twu.biblioteca.presentation.Messages;
+import com.twu.biblioteca.presentation.View;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class DisplayListOfUsersTest {
+
+    @Test
+    public void shouldCallDisplayListOfUsers() {
+        View view = mock(View.class);
+        ArrayList<User> users = mock(ArrayList.class);
+
+        DisplayListOfUsers displayListOfUsers = new DisplayListOfUsers(view, users, Messages.listOfUsers, Messages.userHeader);
+        displayListOfUsers.execute();
+
+        verify(view).displayListOfUsers(users, Messages.listOfUsers, Messages.userHeader);
+    }
+}
